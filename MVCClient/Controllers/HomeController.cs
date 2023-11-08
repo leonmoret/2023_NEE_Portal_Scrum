c700ef19964bfa47018406dad5059558682290a9
unknown <leon@abclic.ch>
2023-11-08 19:38:10 +0100

    display marker on the map

---
+++ MVCClient/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
